3f957b47e02db6bad5c5ae5373fba2469bb8326d
taninesi <aelezaj@live.com>
2016-09-04 17:42:26 -0400

    Initial commit

---
+++ src/nyc/c4q/nesada/Main.java
@@ -0,0 +1,42 @@
+/*
+
+Given an array of strings and an integer, write a method that returns an ArrayList where all the strings of the given length are omitted.
+
+wordsWithoutList({"a", "bb", "b", "ccc"}, 1) → {"bb", "ccc"}
+wordsWithoutList({"a", "bb", "b", "ccc"}, 3) → {"a", "bb", "b"}
+wordsWithoutList({"a", "bb", "b", "ccc"}, 4) → {"a", "bb", "b", "ccc"}
+
+ */
+
+
+package nyc.c4q.nesada;
+
+import  java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> letters = new ArrayList<String>(Arrays.asList("a","bb","b","ccc"));
+
+        System.out.println(takeOf(letters, 4));
+    }
+
+    public static ArrayList<String> takeOf(ArrayList el, int a){
+
+        String currentEl = "";
+        for (int i = 0; i<el.size(); i++){
+
+            currentEl = el.get(i).toString();
+
+            if (currentEl.length() == a){
+            el.remove(i);
+            }
+
+        }
+
+        return el;
+    }
+
+
+}
